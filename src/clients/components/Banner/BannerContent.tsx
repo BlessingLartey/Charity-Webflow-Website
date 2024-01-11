d71663c29338a5529eb5b0af35e0434072cb2936
Redeemm <redeemerdela419@gmail.com>
2024-01-11 12:38:26 +0000

    r

--- src/clients/components/Banner/BannerContent.tsx
+++ src/clients/components/Banner/BannerContent.tsx
@@ -1,7 +1,15 @@
+import banner from '../../assets/images/banner.png'
 function BannerContent() {
   return (
     <>
-      <div className="bannerOuter">
+      <div className="bannerOuter bg-slate-600 m-0 w-[100%]">
+        <div className="">
+        {/* <img
+              className="w-3 cursor-pointer"
+              src={banner}
+              alt="phone"
+            /> */}
+        </div>
         <div className="banner">Being Life Saver For Someone</div>
       </div>
     </>
--- src/clients/components/Header/Header.tsx
+++ src/clients/components/Header/Header.tsx
@@ -23,35 +23,35 @@ function Header() {
             >
               <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
                 <li>
-                  <a className="hover:text-gray-500" href="#">
+                  <a className="hover:text-gray-500 text-sm" href="#">
                     Home
                   </a>
                 </li>
                 <li>
-                  <a className="hover:text-gray-500" href="#">
+                  <a className="hover:text-gray-500 text-sm" href="#">
                     About Us
                   </a>
                 </li>
                 <li>
-                  <a className="hover:text-gray-500" href="#">
+                  <a className="hover:text-gray-500 text-sm" href="#">
                     Pages
                   </a>
                 </li>
                 <li>
-                  <a className="hover:text-gray-500" href="#">
+                  <a className="hover:text-gray-500 text-sm" href="#">
                     Events
                   </a>
                 </li>
                 <li>
-                  <a className="hover:text-gray-500" href="#">
+                  <a className="hover:text-gray-500 text-sm" href="#">
                     Contacts
                   </a>
                 </li>
               </ul>
             </div>
             <div className="flex items-center">
-              <button className="bg-[#FF6D6D] text-white px-5 py-2 rounded-full hover:bg-[#fa5a5a]">
-                Sign in
+              <button className="bg-[#FF6D6D] font-work_sans text-base text-white px-5 py-2 rounded-full hover:bg-[#fa5a5a]">
+                Donate
               </button>
             </div>
           </nav>
